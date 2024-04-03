94087c0d8ff774da20f34c889a0e0cd03cd09444
ILÁN AVERBUCH <47756465@ort>
2024-04-03 09:06:59 -0300

    commit

---
+++ codigo.js
@@ -0,0 +1,57 @@
+let listToDo = [];
+  function addTodo() {
+    const toDoInput = document.getElementById("todoInput").value
+    if (toDoInput !== "") {
+      const time = new Date().getTime();
+      const toDo = {
+        tarea: toDoInput,
+        horaCreado: time,
+        completa: null
+      };
+      listToDo.push(toDo);
+      updateTodoList();
+      toDoInput.value = "";
+    }
+  }
+  
+  function updateTodoList() {
+    const todoList = document.getElementById("todoList");
+    todoList.innerHTML = "";
+      listToDo.forEach(toDo => {
+        const li = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.addEventListener("change", () => {
+          if (checkbox.checked) {
+            toDo.completa = new Date().getTime();
+          } else {
+            toDo.completa = null;
+          }
+          updateTodoList();
+        });
+      
+      const label = document.createElement("label");
+      label.textContent = toDo.tarea + " (creado el " + new Date(toDo.horaCreado).toLocaleString() + ")";
+      if (toDo.completa) {
+        label.style.textDecoration = "line-through";
+        label.textContent += " - completado el " + new Date(toDo.completa).toLocaleString();
+        checkbox.checked = true;
+      }
+      li.appendChild(checkbox);
+      li.appendChild(label);
+      todoList.appendChild(li);
+    });
+  }
+  function showFastestTask() {
+    const completedTasks = listToDo.filter(toDo => toDo.completa !== null);
+    if (completedTasks.length > 0) {
+      const fastestTask = completedTasks.reduce((prev, current) => {
+        return prev.completa - prev.horaCreado < current.completa - current.horaCreado ? prev : current;
+      });
+      const seconds = Math.round((fastestTask.completa - fastestTask.horaCreado) / 1000);
+      alert("La tarea más rápida fue '" + fastestTask.tarea + "', completada en " + seconds + " segundos.");
+    } else {
+      alert("No hay tareas completadas aún.");
+    }
+  }
+  
